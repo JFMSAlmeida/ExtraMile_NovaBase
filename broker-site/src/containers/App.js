32aa04f1b6c8aa96c1d660dee71dffcccf2ad6e3
Samuel Santos <samuel.c.santos@tecnico.ulisboa.pt>
2018-07-04 23:01:59 +0100

    fetch working #2

---
+++ broker-site/src/containers/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import './App.css';
+
+//const client = require('./client');
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        // This binding is necessary to make `this` work in the callback
+        this.handleClickProcess = this.handleClickProcess.bind(this);
+        this.handleClickEcho = this.handleClickEcho.bind(this);
+    }
+
+    handleClickProcess() {
+        fetch('http://localhost:8083/rest/brokers/process?param1=B100&param2=B1001')
+        .then(response => {
+            console.log(response.text);
+            return response.text();
+        })
+        .then(body => {
+            console.log(JSON.parse(body));
+        });
+    }
+
+    handleClickEcho() {
+        fetch('http://localhost:8083/rest/brokers/echo?request=ola')
+            .then(response => {
+                console.log(response.text);
+                return response.text();
+            })
+            .then(body => {
+                console.log(JSON.parse(body));
+            });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>HELLO THIS IS A TEST</h1>
+                <button onClick={this.handleClickProcess}>Click Process</button>
+                <button onClick={this.handleClickEcho}>Click Echo</button>
+            </div>
+        );
+    }
+}
+
+export default App;
--- broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/services/remote/BrokerRestController.java
+++ broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/services/remote/BrokerRestController.java
@@ -8,19 +8,48 @@ import org.springframework.web.bind.annotation.*;
 import pt.ulisboa.tecnico.softeng.broker.services.local.BrokerInterface;
 import pt.ulisboa.tecnico.softeng.broker.exception.BrokerException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping(value = "/rest/brokers")
 public class BrokerRestController {
 	private static Logger logger = LoggerFactory.getLogger(BrokerRestController.class);
 
-	@RequestMapping(value = "/process", method = RequestMethod.POST)
-	public String process(@RequestParam(value="param1") String brokerCode,
-						  @RequestParam(value="param2") String id) {
+	@CrossOrigin
+	@RequestMapping(value = "/process")
+	public ResponseEntity<Map<String, Object>> process(@RequestParam(value="param1") String brokerCode,
+													   @RequestParam(value="param2") String id) {
 		try {
 			BrokerInterface.processAdventure(brokerCode, id);
-			return "ok";
+
+			Map<String, Object> message = new HashMap<String, Object>();
+			message.put("location", "/rest/brokers");
+			message.put("summary", "ok");
+			message.put("code", 200);
+			Map<String, Object> json = new HashMap<String, Object>();
+			json.put("success", true);
+			json.put("message", message);
+			return new ResponseEntity<Map<String, Object>>(json, HttpStatus.OK);
+
 		} catch (BrokerException be) {
-			return "not ok";
+			Map<String, Object> message = new HashMap<String, Object>();
+			message.put("location", "/rest/brokers");
+			message.put("summary", "error");
+			message.put("code", 500);
+			Map<String, Object> json = new HashMap<String, Object>();
+			json.put("success", false);
+			json.put("message", message);
+			return new ResponseEntity<Map<String, Object>>(json, HttpStatus.OK);
 		}
+	}
+
+	@CrossOrigin
+	@RequestMapping(value = "/echo")
+	public ResponseEntity<Map<String, Object>> echo(@RequestParam(value="request", defaultValue="Echo") String request) {
+		Map<String, Object> json = new HashMap<String, Object>();
+		json.put("success", false);
+		json.put("message", request);
+		return new ResponseEntity<Map<String, Object>>(json, HttpStatus.OK);
 	}
 }
