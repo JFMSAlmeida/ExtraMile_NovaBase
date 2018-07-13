2960b05785c12fb78f1d6a05fb972319be0b367b
Samuel Santos <samuel.c.santos@tecnico.ulisboa.pt>
2018-07-13 22:44:44 +0100

    closes refactor options

--- broker-site/src/components/Transaction.js
+++ broker-site/src/components/Transaction.js
@@ -17,10 +17,29 @@ class Transaction extends Component {
             );
         });
 
+        console.log(transactions.length);
         return (
-            <React.Fragment>
-                {transactionsArray}
-            </React.Fragment>
+            <div>
+                {this.props.couldFetchTransactions ?
+                    (transactions.length !== 0) ?
+                        <React.Fragment>
+                            {transactionsArray}
+                        </React.Fragment>
+                        :
+                        <span>
+                            No transactions to show.
+                        </span>
+                    :
+                    (transactions.length !== 0) ?
+                        <span>
+                            Invalid Iban
+                        </span>
+                        :
+                        <span>
+                            Bank Server Down
+                        </span>
+                }
+            </div>
         )
 
     }
